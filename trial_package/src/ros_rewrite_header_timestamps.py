231a100466d31480570564402ba9c4b567be59f4
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-07-11 14:56:12 +0200

    edit timestamp script

--- trial_package/src/ros_rewrite_header_timestamps.py
+++ trial_package/src/ros_rewrite_header_timestamps.py
@@ -1,12 +1,16 @@
 import rosbag
 import argparse
 
-with rosbag.Bag('output.bag', 'w') as outbag:
-    inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220622_sequence_6.bag','r')
+with rosbag.Bag('output1.bag', 'w') as outbag:
+    inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220705_sequence_108.bag','r')
     for topic, msg, t in inbag.read_messages():
-        # This also replaces tf timestamps under the assumption 
+        # This also replaces tf timestamps under the assumption
         # that all transforms in the message share the same timestamp
-        if topic == "/Bebop1/pose" and msg.posestamped:
-            outbag.write(topic, msg, msg.transforms[0].header.stamp)
-        else:
-            outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+        if topic == "/Bebop1/position_velocity_orientation_estimation" and msg.pose:
+        #   print(msg.odometry)
+            print(msg.header)
+
+            print(t)
+           # outbag.write(topic,msg,msg.Odometry[0].header.stamp)
+        #else:
+        #    print('sacre bleu...')
