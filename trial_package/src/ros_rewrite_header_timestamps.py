5a2c85635f727a1168ebe8245e2f5591b6bf90ee
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-07-12 14:06:26 +0200

    update rewrite header script

--- trial_package/src/ros_rewrite_header_timestamps.py
+++ trial_package/src/ros_rewrite_header_timestamps.py
@@ -1,18 +1,62 @@
 import rosbag
 import argparse
+import os
 
-with rosbag.Bag('output_rewrite_stamp_with_t.bag', 'w') as outbag:
-    inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220705_sequence_108.bag','r')
-    target_topic='/Husky/Pose'
-    for topic, msg, t in inbag.read_messages():
-        # This also replaces tf timestamps under the assumption
-        # that all transforms in the message share the same timestamp
-        if topic == "/Bebop1/position_velocity_orientation_estimation" and msg.pose:
-            #print(msg.header.stamp.nsecs)
-            time_sec =t.secs
-            time_nano=t.nsecs
-            msg.header.stamp.secs = time_sec
-            outbag.write(target_topic,msg,t)
-            #outbag.write(target_topic,msg,msg.header.stamp)
-        #else:
-        #    print('sacre bleu...')
+def main():
+    """Rewrite bagfile header stamps to syncronize the zed camera with optitrack data
+	"""
+	parser = argparse.ArgumentParser(description="Rewrite header stamps and put them in new bag file")
+
+	parser.add_argument("--source_dir", help="Directory containing rosbags.")
+	parser.add_argument("--target_dir", help="Output directory.")
+	# parser.add_argument("--instance", type=int, help="Start index number.")
+	parser.add_argument("--end", type=int, help="End index number.")
+	parser.add_argument("--date", help="Filename date prefix.")
+
+	args = parser.parse_args()
+
+    source_dir = args.source_dir
+    target_dir = args.target_dir
+
+    print("Extract data from the directory %s into the directory %s" %(source_dir,target_dir))
+    
+    
+    with rosbag.Bag(os.path.join(target_dir,"%s_sequence_%01i_edit.bag" % (args.date,i)), 'w') as outbag:
+        
+        filepath = os.path.join(source_dir,"%s_sequence_%01i.bag" % (args.date,i))
+        
+        inbag=rosbag.Bag(filepath)
+        
+        # inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220705_sequence_108.bag','r')
+        
+        # target_topic='/%s/Pose' % (args.instance)
+        husky_topic='/Husky/Pose'
+        object_topic='/Box/Pose'
+
+        for topic, msg, t in inbag.read_messages():
+            # This also replaces tf timestamps under the assumption
+            # that all transforms in the message share the same timestamp
+            if topic == "/Bebop1/position_velocity_orientation_estimation" and msg.pose:
+                #print(msg.header.stamp.nsecs)
+                time_sec =t.secs
+                time_nano=t.nsecs
+                # Rewrite correct header stamps to the 'faulty' Husky stamps, since that was the main machine on which was recorded, so that is easier.
+                msg.header.stamp.secs  = time_sec
+                msg.header.stamp.nsecs = time_nano
+                outbag.write(husky_topic,msg,t)
+            
+            if topic== "/Bebop2/position_velocity_orientation_estimation" and msg.pose:
+                #print(msg.header.stamp.nsecs)
+                time_sec =t.secs
+                time_nano=t.nsecs
+                # Rewrite correct header stamps to the 'faulty' Husky stamps, since that was the main machine on which was recorded, so that is easier.
+                msg.header.stamp.secs  = time_sec
+                msg.header.stamp.nsecs = time_nano
+                # Write to topic of object, write the (now altered) message at time t
+                outbag.write(object_topic,msg,t)
+            
+            if topic== "/zed_node/left/camera_info_throttle":
+                print('/zed_node msg is: \n',msg)
+
+if __name__ == "__main__":
+	main()
