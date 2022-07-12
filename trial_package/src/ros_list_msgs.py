bbd53ef8aa360acdd0261c630f7f18b57122c46e
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-07-12 09:18:26 +0200

    update rospy scripts

--- trial_package/src/ros_list_msgs.py
+++ trial_package/src/ros_list_msgs.py
@@ -1,10 +1,12 @@
 import rosbag
 import argparse
 
-num_msgs=500
-inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220622_sequence_6.bag','r')
-with rosbag.Bag('output.bag', 'w') as outbag:
+num_msgs=10
+# inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220622_sequence_6.bag','r')
+inbag=rosbag.Bag('/home/pmvanderburg/output_msg_header_stamp.bag','r')
+with rosbag.Bag('output_trial.bag', 'w') as outbag:
     for topic, msg, t in inbag.read_messages():
         while num_msgs:
-            outbag.write(topic, msg, t)
+            print('time t is: ',t, '\n and message is:\n',msg)
+            #outbag.write(topic, msg, t)
             num_msgs -= 1
--- trial_package/src/ros_rewrite_header_timestamps.py
+++ trial_package/src/ros_rewrite_header_timestamps.py
@@ -1,16 +1,18 @@
 import rosbag
 import argparse
 
-with rosbag.Bag('output1.bag', 'w') as outbag:
+with rosbag.Bag('output_rewrite_stamp_with_t.bag', 'w') as outbag:
     inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220705_sequence_108.bag','r')
+    target_topic='/Husky/Pose'
     for topic, msg, t in inbag.read_messages():
         # This also replaces tf timestamps under the assumption
         # that all transforms in the message share the same timestamp
         if topic == "/Bebop1/position_velocity_orientation_estimation" and msg.pose:
-        #   print(msg.odometry)
-            print(msg.header)
-
-            print(t)
-           # outbag.write(topic,msg,msg.Odometry[0].header.stamp)
+            #print(msg.header.stamp.nsecs)
+            time_sec =t.secs
+            time_nano=t.nsecs
+            msg.header.stamp.secs = time_sec
+            outbag.write(target_topic,msg,t)
+            #outbag.write(target_topic,msg,msg.header.stamp)
         #else:
         #    print('sacre bleu...')
