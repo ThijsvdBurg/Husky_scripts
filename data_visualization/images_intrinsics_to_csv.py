5a890c26bb4101a7032ba54f4066caffa480cdeb
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-07-12 14:52:44 +0200

    fix indentations

--- data_visualization/images_intrinsics_to_csv.py
+++ data_visualization/images_intrinsics_to_csv.py
@@ -13,34 +13,39 @@ from bagpy import bagreader
 import pandas as pd
 
 def main():
-	"""Extract info from bagfile and append to csv datafile
-	"""
-	parser = argparse.ArgumentParser(description="Extract bagfile to csv")
+    """Extract info from bagfile and append to csv datafile
+    """
+    parser = argparse.ArgumentParser(description="Extract bagfile to csv")
 
-	parser.add_argument("--source_dir", help="Directory containing rosbags.")
-	parser.add_argument("--output_dir", help="Output directory.")
-	parser.add_argument("--start", type=int, help="Start index number.")
-	parser.add_argument("--end", type=int, help="End index number.")
-	parser.add_argument("--date", help="Filename date prefix.")
+    parser.add_argument("--source_dir", help="Directory containing rosbags.")
+    parser.add_argument("--output_dir", help="Output directory.")
+    parser.add_argument("--start", type=int, help="Start index number.")
+    parser.add_argument("--end", type=int, help="End index number.")
+    parser.add_argument("--date", help="Filename date prefix.")
+    parser.add_argument("--edit_suffix", type=bool, help="Filename suffix, empty with raw unedited rosbag")
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	print("Extract data from %s into %s" %(args.source_dir,args.output_dir))
-	# start_nr = 108
-	#max_nr = 19
-	for i in range(args.start, args.end):
-		file = os.path.join(args.source_dir,"%s_sequence_%01i.bag" % (args.date,i))
-		# file = os.path.join(args.source_dir, "20220705_sequence_%01i.bag" % i)
-		b = bagreader(file)
-		# get the list of topics
-		print(b.topic_table)
-		# load data to csv files
-		csvfiles = []
-		for t in b.topics:
-		    data = b.message_by_topic(t)
-			csvfiles.append(data)
-		print(csvfiles[0])
-		data = pd.read_csv(csvfiles[0])
+    print("Extract data from %s into %s" %(args.source_dir,args.output_dir))
+    # start_nr = 108
+    #max_nr = 19
+    for i in range(args.start, args.end):
+        if args.edit_suffix:
+            bagname="%s_sequence_%01i_edit.bag" % (args.date,i)
+        else:
+            bagname="%s_sequence_%01i.bag" % (args.date,i)
+        file = os.path.join(args.source_dir,bagname)
+        # file = os.path.join(args.source_dir, "20220705_sequence_%01i.bag" % i)
+        b = bagreader(file)
+        # get the list of topics
+        print(b.topic_table)
+        # load data to csv files
+        csvfiles = []
+        for t in b.topics:
+            data = b.message_by_topic(t)
+            csvfiles.append(data)
+            print(csvfiles[0])
+            data = pd.read_csv(csvfiles[0])
 
 if __name__ == "__main__":
 	main()
--- trial_package/src/ros_rewrite_header_timestamps.py
+++ trial_package/src/ros_rewrite_header_timestamps.py
@@ -7,24 +7,27 @@ def main():
     """
     parser = argparse.ArgumentParser(description="Rewrite header stamps and put them in new bag file")
     parser.add_argument("--source_dir", help="Directory containing rosbags.")
-	parser.add_argument("--target_dir", help="Output directory.")
-	# parser.add_argument("--start", type=int, help="Start index number.")
-	# parser.add_argument("--end", type=int, help="End index number.")
-	parser.add_argument("--date", help="Filename date prefix.")
+    parser.add_argument("--target_dir", help="Output directory.")
+    # parser.add_argument("--start", type=int, help="Start index number.")
+    # parser.add_argument("--end", type=int, help="End index number.")
+    parser.add_argument("--date", help="Filename date prefix.")
 
-	args = parser.parse_args()
-    start=1
-    end=1
+    args = parser.parse_args()
+    start=108
+    end=108
     source_dir = args.source_dir
     target_dir = args.target_dir
 
     print("Extract data from the directory %s into the directory %s" %(source_dir,target_dir))
 
-    for i in range(start,end):
-        with rosbag.Bag(os.path.join(target_dir,"%s_sequence_%01i_edit.bag" % args.date), 'w') as outbag:
-            
-            filepath = os.path.join(source_dir,"%s_sequence_%01i.bag" % args.date)
+    for i in range(start,end+1):
+        print(i)
+        with rosbag.Bag(os.path.join(target_dir,"%s_sequence_%01i_edit.bag" % (args.date,i)), 'w') as outbag: 
+        #with rosbag.Bag(os.path.join(target_dir,"%s_sequence_edit.bag" % args.date), 'w') as outbag:
 
+            filepath = os.path.join(source_dir,"%s_sequence_%01i.bag" % (args.date,i))
+            #filepath = os.path.join(source_dir,"%s_sequence_.bag" % args.date)
+            print(filepath)
             inbag=rosbag.Bag(filepath)
 
             # inbag=rosbag.Bag('/home/pmvanderburg/noetic-husky/data_acquisition/data_acq_20220622/rostest/20220705_sequence_108.bag','r')
@@ -37,14 +40,14 @@ def main():
                 # This also replaces tf timestamps under the assumption
                 # that all transforms in the message share the same timestamp
                 if topic == "/Bebop1/position_velocity_orientation_estimation" and msg.pose:
-                    #print(msg.header.stamp.nsecs)
+                    print(msg.header.stamp.nsecs)
                     time_sec =t.secs
                     time_nano=t.nsecs
                     # Rewrite correct header stamps to the 'faulty' Husky stamps, since that was the main machine on which was recorded, so that is easier.
                     msg.header.stamp.secs  = time_sec
                     msg.header.stamp.nsecs = time_nano
                     outbag.write(husky_topic,msg,t)
-                
+
                 if topic== "/Bebop2/position_velocity_orientation_estimation" and msg.pose:
                     #print(msg.header.stamp.nsecs)
                     time_sec =t.secs
@@ -54,10 +57,10 @@ def main():
                     msg.header.stamp.nsecs = time_nano
                     # Write to topic of object, write the (now altered) message at time t
                     outbag.write(object_topic,msg,t)
-                
+
                 if topic== "/zed_node/left/camera_info_throttle":
                     print('/zed_node msg is: \n',msg)
-        +=i
+        # +=i
         print(i)
 
 if __name__ == "__main__":
