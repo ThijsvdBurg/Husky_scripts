e777f0936e5249cca50bcdbb6042a8b571f91218
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-07-13 16:12:03 +0200

    bugfixes

--- trial_package/src/scripts/ros_rewrite_header_timestamps_debug.py
+++ trial_package/src/scripts/ros_rewrite_header_timestamps_debug.py
@@ -18,6 +18,7 @@ def main():
     end=args.end
     source_dir = args.source_dir
     target_dir = args.target_dir
+
     if args.suffix==None:
         suffix=""
     else:
@@ -26,20 +27,20 @@ def main():
     print("\nExtract data from the directory %s into the directory %s" %(source_dir,target_dir))
 
     for i in range(start,end+1):
-        print(i)
+        # print(i)
         outbagpath=os.path.join(target_dir,"%s_sequence_%01i_edit.bag" % (args.date,i))
         # print("Outbagpath is: \n",outbagpath)
         if os.path.exists(outbagpath):
             print("\nOutbagpath exists, skipping %s_sequence_%01i_edit.bag \n" % (args.date,i))
         else:
-            with rosbag.Bag(outbagpath,'w') as outbag:
-            #with rosbag.Bag(os.path.join(target_dir,"%s_sequence_edit.bag" % args.date), 'w') as outbag:
-
-                filepath = os.path.join(source_dir,"%s_sequence_%01i%s.bag" % (args.date,i,suffix))
-                # print("Inbagpath",filepath)
-                if not os.path.exists(filepath):
-                    print("\nInbagpath does not exist, skipping %s_sequence_%01i%s.bag \n" % (args.date,i,suffix))
-                else:
+            # check if sourge bagfile exists first before creating a target bagfile object
+            filepath = os.path.join(source_dir,"%s_sequence_%01i%s.bag" % (args.date,i,suffix))
+            if not os.path.exists(filepath):
+                print("\nInbagpath does not exist, skipping %s_sequence_%01i%s.bag \n" % (args.date,i,suffix))
+            else:
+                with rosbag.Bag(outbagpath,'w') as outbag:
+                    #with rosbag.Bag(os.path.join(target_dir,"%s_sequence_edit.bag" % args.date), 'w') as outbag:
+                    # print("Inbagpath",filepath)
                     inbag=rosbag.Bag(filepath)
                     for topic, msg, t in inbag.read_messages():
                         print("time t is", t)
