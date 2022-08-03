14d4f8029d3105bd76bc0025519965c138a5a5cf
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-08-03 11:42:48 +0200

    add slop as argument

--- trial_package/src/scripts/camera_pose_sync.py
+++ trial_package/src/scripts/camera_pose_sync.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
 import rospy
+import argparse
 import message_filters
 from sensor_msgs.msg import Image, CameraInfo
 import nav_msgs.msg
+
 # import geometry_msgs
+
+parser = argparse.ArgumentParser(description="Sync rosbag topics and rewrite the synced messages into a new rosbag")
+parser.add_argument("--slop", type=float, help="Directory containing rosbags.")
+args = parser.parse_args()
 
 # When topics are synced, they should be put in a new bag file together
 def callback(leftcam,rightcam,robotpos,objectpos):
   # rospy.loginfo('Match found')
   rospy.loginfo(rospy.get_caller_id() + ' I heard %s', leftcam.header)
   rospy.loginfo(rospy.get_caller_id() + ' I heard %s', robotpos.header)
-  
+
 
 left_sub = message_filters.Subscriber('/zed_node/left/camera_info_throttle', CameraInfo)
 right_sub = message_filters.Subscriber('/zed_node/right/camera_info_throttle', CameraInfo)
@@ -20,7 +26,7 @@ object_sub = message_filters.Subscriber('/Box/Pose', nav_msgs.msg.Odometry)
 rospy.init_node('camera_synchronizer_node',anonymous=True)
 
 # ts = message_filters.ApproximateTimeSynchronizer([info1_sub, info2_sub, object_pose_sub], queue_size=10, slop=10)
-ts = message_filters.ApproximateTimeSynchronizer([left_sub, right_sub, robot_sub, object_sub], queue_size=10, slop=0.01)
+ts = message_filters.ApproximateTimeSynchronizer([left_sub, right_sub, robot_sub, object_sub], queue_size=10, slop=args.slop)
 #ts = message_filters.TimeSynchronizer([info1_sub, info2_sub],queue_size=1)
 #ts = message_filters.TimeSynchronizer([object_sub, robot_sub],queue_size=10)
 #ts = message_filters.ApproximateTimeSynchronizer([object_sub, robot_sub],queue_size=10,slop=1)
