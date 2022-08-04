dad10596c30109d71c86f9d61b1981c84573293e
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-08-04 09:56:46 +0200

    try to add publisher to camera_pose_sync script

--- trial_package/src/scripts/camera_pose_sync.py
+++ trial_package/src/scripts/camera_pose_sync.py
@@ -12,12 +12,17 @@ parser = argparse.ArgumentParser(description="Sync rosbag topics and rewrite the
 parser.add_argument("--slop", type=float, default=0.05, help="Directory containing rosbags.")
 args = parser.parse_args()
 
+pub = rospy.Publisher('/status', String, queue_size=1000)
+
 # When topics are synced, they should be put in a new bag file together
 def callback(leftcam,rightcam,robotpos,objectpos):
   # rospy.loginfo('Match found')
+  last_data=objectpos.pose
   rospy.loginfo(rospy.get_caller_id() + ' I heard %s', leftcam.header)
   rospy.loginfo(rospy.get_caller_id() + ' I heard %s', robotpos.header)
   rospy.loginfo(rospy.get_caller_id() + ' I heard %s', robotpos.pose)
+  pub.publish(last_data)
+  print('last data published on /status topic')
 
 left_sub = message_filters.Subscriber('/zed_node/left/camera_info_throttle', CameraInfo)
 right_sub = message_filters.Subscriber('/zed_node/right/camera_info_throttle', CameraInfo)
@@ -25,12 +30,8 @@ robot_sub = message_filters.Subscriber('/Husky/Pose', nav_msgs.msg.Odometry)
 object_sub = message_filters.Subscriber('/Box/Pose', nav_msgs.msg.Odometry)
 rospy.init_node('camera_synchronizer_node',anonymous=True)
 
-# ts = message_filters.ApproximateTimeSynchronizer([info1_sub, info2_sub, object_pose_sub], queue_size=10, slop=10)
 ts = message_filters.ApproximateTimeSynchronizer([left_sub, right_sub, robot_sub, object_sub], queue_size=10, slop=args.slop)
 #ts = message_filters.TimeSynchronizer([info1_sub, info2_sub],queue_size=1)
-#ts = message_filters.TimeSynchronizer([object_sub, robot_sub],queue_size=10)
-#ts = message_filters.ApproximateTimeSynchronizer([object_sub, robot_sub],queue_size=10,slop=1)
 
-#, object_pose_sub], 10)
 ts.registerCallback(callback)
 rospy.spin()
