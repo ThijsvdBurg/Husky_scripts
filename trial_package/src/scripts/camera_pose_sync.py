0238dc3320af5fa9d0d83002ba9318fcbcf81f25
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-08-04 13:34:32 +0200

    Add Image topics to camera_pose_sync

--- trial_package/src/scripts/camera_pose_sync.py
+++ trial_package/src/scripts/camera_pose_sync.py
@@ -15,33 +15,39 @@ parser = argparse.ArgumentParser(description="Sync rosbag topics and rewrite the
 parser.add_argument("--slop", type=float, default=0.05, help="Directory containing rosbags.")
 args = parser.parse_args()
 
-leftcampub = rospy.Publisher('/sync/left/camera_info', CameraInfo, queue_size=1000)
-rightcampub = rospy.Publisher('/sync/right/camera_info', CameraInfo, queue_size=1000)
+leftImpub = rospy.Publisher('/sync/left/image_rect', Image, queue_size=1000)
+leftInfopub = rospy.Publisher('/sync/left/camera_info', CameraInfo, queue_size=1000)
+rightImpub = rospy.Publisher('/sync/right/image_rect', Image, queue_size=1000)
+rightInfopub = rospy.Publisher('/sync/right/camera_info', CameraInfo, queue_size=1000)
 robotpub = rospy.Publisher('/sync/robot/pose', nav_msgs.msg.Odometry, queue_size=1000)
 objectpub = rospy.Publisher('/sync/object/pose', nav_msgs.msg.Odometry, queue_size=1000)
 
 # When topics are synced, they should be put in a new bag file together
-def callback(leftcam,rightcam,robotpos,objectpos):
+def callback(leftcaminfo,leftcamim,rightcaminfo,rightcamim,robotpos,objectpos):
   # rospy.loginfo('Match found')
   # last_data=objectpos.pose
   # rospy.loginfo(rospy.get_caller_id() + ' I heard %s', leftcam.header)
   # rospy.loginfo(rospy.get_caller_id() + ' I heard %s', robotpos.header)
   # rospy.loginfo(rospy.get_caller_id() + ' I heard %s', robotpos.pose)
   # pub.publish(last_data)
-  leftcampub.publish(leftcam)
-  rightcampub.publish(rightcam)
+  leftImpub.publish(leftcaminfo)
+  leftInfopub.publish(leftcamim)
+  rightImpub.publish(rightcaminfo)
+  rightInfopub.publish(rightcamim)
   robotpub.publish(robotpos)
   objectpub.publish(objectpos)
 
-  print('last data published on /status topic')
+  print('last data published')
 
-left_sub = message_filters.Subscriber('/zed_node/left/camera_info_throttle', CameraInfo)
-right_sub = message_filters.Subscriber('/zed_node/right/camera_info_throttle', CameraInfo)
+leftinfo_sub = message_filters.Subscriber('/zed_node/left/camera_info_throttle', CameraInfo)
+leftimage_sub = message_filters.Subscriber('/zed_node/left/image_rect_color_throttle', Image)
+rightinfo_sub = message_filters.Subscriber('/zed_node/right/camera_info_throttle', CameraInfo)
+rightimage_sub = message_filters.Subscriber('/zed_node/right/image_rect_color_throttle', Image)
 robot_sub = message_filters.Subscriber('/Husky/Pose', nav_msgs.msg.Odometry)
 object_sub = message_filters.Subscriber('/Box/Pose', nav_msgs.msg.Odometry)
 rospy.init_node('camera_synchronizer_node',anonymous=True)
 
-ts = message_filters.ApproximateTimeSynchronizer([left_sub, right_sub, robot_sub, object_sub], queue_size=10, slop=args.slop)
+ts = message_filters.ApproximateTimeSynchronizer([leftinfo_sub, leftimage_sub, rightinfo_sub, rightimage_sub, robot_sub, object_sub], queue_size=30, slop=args.slop)
 #ts = message_filters.TimeSynchronizer([info1_sub, info2_sub],queue_size=1)
 
 ts.registerCallback(callback)
