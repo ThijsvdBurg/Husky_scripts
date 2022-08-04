fdcc6466487eb2062d86bdb8fa18b38d0a9ac8ed
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-08-04 16:28:18 +0200

    add support for 001 002 suffices

--- data_visualization/bag_to_csv_thijs_test.py
+++ data_visualization/bag_to_csv_thijs_test.py
@@ -16,12 +16,13 @@ def main():
     """Extract info from bagfile and append to csv datafile
     """
     parser = argparse.ArgumentParser(description="Extract bagfile to csv")
-    parser.add_argument("--source_dir", help="Directory containing rosbags. Absolute paths are preferred")
+
+    parser.add_argument("--source_dir", help="Directory containing rosbags.")
     parser.add_argument("--output_dir", help="Output directory.")
-    parser.add_argument("--start", type=int, help="Start index number: 001 to 099 are just denoted as 1 to 99")
-    parser.add_argument("--end", type=int, help="End index number; same syntax as start index number")
+    parser.add_argument("--start", type=int, help="Start index number.")
+    parser.add_argument("--end", type=int, help="End index number.")
     parser.add_argument("--date", help="Filename date prefix.")
-    parser.add_argument("--suffix", help="Filename suffix, either edit, crop, synced or omit argument for unedited rosbag")
+    parser.add_argument("--suffix", help="Filename suffix, edit, crop, synced or empty for raw unedited rosbag")
 
     args = parser.parse_args()
 
@@ -30,25 +31,17 @@ def main():
     width=len(str(max_num))
 
     for i in range(args.start, args.end+1):
-
-        # select the proper bagname depending on whether a suffix is supplied in the arguments
         if args.suffix==None:
             bagname="%s_sequence_{i:0{width}}.bag".format(i=i,width=width) %(args.date)
         else:
             bagname="%s_sequence_{i:0{width}}_%s.bag".format(i=i,width=width) %(args.date, args.suffix)
+        #bagname="%s_sequence_.bag" % (args.date,i)
+        #property = 'name'
+        #formatter = "my {} is {{}}".format(property) # => "my name is {}"
+        #print formatter.format('wong')
+
         file = os.path.join(args.source_dir,bagname)
         print(file)
-        b = bagreader(file)
-
-        # get the list of topics
-        print(b.topic_table)
-        # load data to csv files
-        csvfiles = []
-        for t in b.topics:
-            data = b.message_by_topic(t)
-            csvfiles.append(data)
-            print(csvfiles[0])
-            data = pd.read_csv(csvfiles[0])
 
 if __name__ == "__main__":
 	main()
