06f17f498d7474188639e73d887b96b592e355ce
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-08-04 10:36:45 +0200

    debug

--- trial_package/src/scripts/camera_pose_sync.py
+++ trial_package/src/scripts/camera_pose_sync.py
@@ -4,8 +4,8 @@ import rospy
 import argparse
 import message_filters
 from sensor_msgs.msg import Image, CameraInfo
-# import nav_msgs.msg
-from nav_msgs.msg import Odometry
+import nav_msgs.msg
+# from nav_msgs.msg import Odometry
 from std_msgs.msg import String
 # from geometry_msgs.msg import PoseWithCovariance
 
@@ -17,8 +17,8 @@ args = parser.parse_args()
 
 leftcampub = rospy.Publisher('/sync/left/camera_info', CameraInfo, queue_size=1000)
 rightcampub = rospy.Publisher('/sync/right/camera_info', CameraInfo, queue_size=1000)
-robotpub = rospy.Publisher('/sync/robot/pose', Odometry, queue_size=1000)
-objectpub = rospy.Publisher('/sync/object/pose', Odometry, queue_size=1000)
+robotpub = rospy.Publisher('/sync/robot/pose', nav_msgs.msg.Odometry, queue_size=1000)
+objectpub = rospy.Publisher('/sync/object/pose', nav_msgs.msg.Odometry, queue_size=1000)
 
 # When topics are synced, they should be put in a new bag file together
 def callback(leftcam,rightcam,robotpos,objectpos):
@@ -37,8 +37,8 @@ def callback(leftcam,rightcam,robotpos,objectpos):
 
 left_sub = message_filters.Subscriber('/zed_node/left/camera_info_throttle', CameraInfo)
 right_sub = message_filters.Subscriber('/zed_node/right/camera_info_throttle', CameraInfo)
-robot_sub = message_filters.Subscriber('/Husky/Pose', Odometry)
-object_sub = message_filters.Subscriber('/Box/Pose', Odometry)
+robot_sub = message_filters.Subscriber('/Husky/Pose', nav_msgs.msg.Odometry)
+object_sub = message_filters.Subscriber('/Box/Pose', nav_msgs.msg.Odometry)
 rospy.init_node('camera_synchronizer_node',anonymous=True)
 
 ts = message_filters.ApproximateTimeSynchronizer([left_sub, right_sub, robot_sub, object_sub], queue_size=10, slop=args.slop)
