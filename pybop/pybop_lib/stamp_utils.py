58686bb3876a678911fff483efa5819bec834322
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-10-03 16:27:04 +0200

    pyboplib mods

--- pybop/pybop_lib/stamp_utils.py
+++ pybop/pybop_lib/stamp_utils.py
@@ -12,8 +12,8 @@
 import rosbag
 # import sys
 
-def get_Timestamp(bagpath,topic_name):
-  """Extract an image sequence from a rosbag.
+def getFirstStamp(bagpath,topic_name):
+  """Extract the first header stamp and the number of messages in a topic.
   """
 
   bag = rosbag.Bag(bagpath, "r")
@@ -23,19 +23,18 @@ def get_Timestamp(bagpath,topic_name):
     if msg.header.seq == 1:
       first_timestamp = msg.header.stamp
     count+=1
-    if count == 1:
-      break
-    bag.close()
-
-  return first_timestamp
+    #print('')
+  bag.close()
+  return first_timestamp, count
 
 
 def assertStamp(current_stamp, first_stamp, bound):
   upperb = first_stamp+bound
   lowerb = first_stamp-bound
   print('first timestamp in bag is:', first_stamp, ' and the current stamp is: ', current_stamp)
+  print('lowerbound and upperbound are:', lowerb, upperb)
   if current_stamp < lowerb:
-    print('The current stamp is lower than expected, try sleepbf:=2.0')
+    print('The current stamp is lower than expected, \n\n try increasing sleepbf, default is 1.7 \n\n')
   if current_stamp > upperb:
-    print('The current stamp is higher than expected, try sleepbf:=1.4')
+    print('The current stamp is higher than expected, \n\n\ try decreasing sleepbf, default is 1.7 \n\n')
   assert current_stamp > lowerb and current_stamp < upperb
