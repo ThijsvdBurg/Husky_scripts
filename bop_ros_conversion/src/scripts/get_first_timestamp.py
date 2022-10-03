f22f4c8c94a0be0899a2e792b74ff706e9a5481f
ThijsvdBurg <thijsvdburg95@gmail.com>
2022-10-03 12:06:01 +0200

    edit get timestamp script

--- bop_ros_conversion/src/scripts/get_first_timestamp.py
+++ bop_ros_conversion/src/scripts/get_first_timestamp.py
@@ -9,7 +9,6 @@
 
 import os
 import argparse
-import cv2
 import rosbag
 import sys
 
@@ -19,35 +18,35 @@ def main():
   parser = argparse.ArgumentParser(description="Extract images from a ROS bag.")
 
   parser.add_argument("--bag_file", help="Input ROS bag.")
-  parser.add_argument("--output_dir", help="Output directory.")
+  #parser.add_argument("--output_dir", help="Output directory.")
   parser.add_argument("--image_topic", help="Image topic.")
   # parser.add_argument("--intrinsics_topic", help="Topic containing CameraInfo.")
 
-args = parser.parse_args()
+  args = parser.parse_args()
 
-print("Extract images from %s on topic %s into %s" % (args.bag_file,args.image_topic, args.output_dir))
+  print("Extract images from %s on topic %s into %s" % (args.bag_file,args.image_topic, args.output_dir))
 
-bag = rosbag.Bag(args.bag_file, "r")
-bridge = CvBridge()
-if not os.access(args.output_dir,os.W_OK)==True:
-print('target dir not writable, making the directory?')
-if input("Do You Want To Continue? [y/n]") == "y":
-os.mkdir(args.output_dir)
-print("created output directory")
-else:
-print('aborting')
-sys.exit()
-count = 0
+  bag = rosbag.Bag(args.bag_file, "r")
+  
+  if not os.access(args.output_dir,os.W_OK)==True:
+    print('target directory not available/writable, requiring user input...')
+  if input("Do You Want me to create the directory? [y/n]") == "y":
+    os.mkdir(args.output_dir)
+    print("created output directory")
+  else:
+    print('aborting')
+    sys.exit()
+  
+  count = 0
+  for topic, msg, t in bag.read_messages(topics=[args.topic]):
+    if topic.header.seq == 1:
+      first_timestamp = topic.header.stamp
+    count+=1
+    if count == 1:
+      break
+    bag.close()
 
-for topic, msg, t in bag.read_messages(topics=[args.image_topic]):
-cv2.imwrite(os.path.join(args.output_dir, "%06i.png" % count), cv_image)
-print("Wrote image %i" % count)
-count+=1
-if count == 1:
-  break
-bag.close()
-
-return
+  return first_timestamp
 
 if __name__ == '__main__':
   main()
